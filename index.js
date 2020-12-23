5e5bdaa8ef6b1ea64d8dc189230aa3fe424d8c3e
mahir2006-star <72641119+mahir2006-star@users.noreply.github.com>
2020-12-23 14:49:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var db=admin.firestore();
 
 const express = require('express')
 const app = express()
-const port = 8080
+const port =process.env.PORT || 8080
 var cors = require('cors')
 app.use(cors());
 const list=[];

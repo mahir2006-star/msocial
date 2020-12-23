e0578f057a93b709f6fb7d7e63e5cd61b9845c92
mahir2006-star <72641119+mahir2006-star@users.noreply.github.com>
2020-12-23 14:09:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+var admin=require("firebase-admin");
+var serviceAccount = require("./mavls-social-firebase-adminsdk-q2pvh-f8a52f8f05.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://mavls-social.firebaseio.com"
+});
+var db=admin.firestore();
+
+const express = require('express')
+const app = express()
+const port = 5000
+var cors = require('cors')
+app.use(cors());
+const list=[];
+db.collection("posts").get().then(function(querySnapshot) {
+    querySnapshot.forEach(function(doc) {
+var jso;
+jso=doc.data();
+jso.docid=doc.id;
+       list.push(jso);
+    });
+app.get('/posts', (reqt, res) => {
+var auth=admin.auth();
+res.json(list);
+});
+});
+app.get('/getuserpost', (reqt, res) => {
+const data=[];
+db.collection("posts").where("userid","==",reqt.query.userid).get().then(function(querySnapshot) {
+    querySnapshot.forEach(function(doc) {
+var jso;
+jso=doc.data();
+jso.docid=doc.id;
+      data.push(jso);
+    });
+res.json(data);
+});
+
+});
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
